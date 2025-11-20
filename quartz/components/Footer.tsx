449529afde1a2ac602c5d9bd9e34bff4b5127cb5
Patrick Thieringer <70220731+pthieringer@users.noreply.github.com>
2025-11-20 15:51:31 -0800

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,8 +13,7 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          <a Patrick Thieringer © {year}
+        <p>Patrick Thieringer © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
