97efb857653de4e5bc3c627b0ac3f9dd1bba6323
Patrick Thieringer <70220731+pthieringer@users.noreply.github.com>
2025-11-21 10:58:03 -0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,7 +30,6 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer({title: "Navigation Menu", 
